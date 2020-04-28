90ca09202df7626562f082911e984286a0edb916
Stanley Stuart <sstuart@salesforce.com>
2020-04-27 23:29:42 -0700

    (feat): break out rule ensuring a11yAudit is actually called to own rule
    This allows us to autofix this case more easily since the ESLint Rule
    guidelines seem to indicate a fix function should be as small as possible.

---
+++ tests/lib/rules/a11y-audit-no-expression.js
@@ -0,0 +1,189 @@
+"use strict";
+
+/**
+ * @fileoverview Tests that `a11yAudit` is called as a function. Prevents accidentally comitting code like
+ * `a11yAudit` (no parens, so the audit doens't actually run)
+ * @author Stanley Stuart <https://github.com/fivetanley>
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/a11y-audit-no-expression');
+const { RuleTester } = require('eslint/lib/rule-tester');
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const TEST_FILE_NAME = 'tests/acceptance/application-test.js';
+const ruleTester = new RuleTester();
+
+ruleTester.run('a11y-audit-no-expression', rule, {
+  valid: [
+    // visit
+    {
+      code: `visit(); a11yAudit();`,
+      filename: TEST_FILE_NAME
+    },
+    // import alias works
+    {
+      code: `import a11yAudit2 from 'ember-a11y-testing/ember-a11y-testing/test-support/audit'; (async () => { visit(); await a11yAudit2; })`,
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018',
+        sourceType: 'module'
+      }
+    },
+    // custom module used
+    {
+      code: 'import { audit } from "dashboard/tests/helpers/audit"; (async () => { while(true) { visit(); await audit(); } })();',
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018',
+        sourceType: 'module'
+      },
+      options: [
+        {
+          auditModule: {
+            package: 'dashboard/tests/helpers/audit',
+            exportName: 'audit'
+          }
+        }
+      ]
+    },
+    // custom module used, default import specifier
+    {
+      code: 'import audit from "dashboard/tests/helpers/audit"; (async () => { while(true) { visit(); await audit(); } })();',
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018',
+        sourceType: 'module'
+      },
+      options: [
+        {
+          auditModule: {
+            package: 'dashboard/tests/helpers/audit',
+            exportName: 'default'
+          }
+        }
+      ]
+    },
+      // custom module used, default import specifier, but copied to another var
+      {
+        code: 'import audit from "dashboard/tests/helpers/audit"; const audit2 = audit; (async () => { while(true) { visit(); await audit2(); } })();',
+        filename: TEST_FILE_NAME,
+        parserOptions: {
+          ecmaVersion: '2018',
+          sourceType: 'module'
+        },
+        options: [
+          {
+            auditModule: {
+              package: 'dashboard/tests/helpers/audit',
+              exportName: 'default'
+            }
+          }
+        ]
+      }
+  ],
+  invalid: [
+    // import aliases work
+    {
+      code: `import a11yAudit24 from 'ember-a11y-testing/test-support/audit'; (async () => { visit(); await a11yAudit24; })()`,
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018',
+        sourceType: 'module'
+      },
+      errors: [{ messageId: 'a11yAuditMustBeCalled' }],
+      output: "import a11yAudit24 from 'ember-a11y-testing/test-support/audit'; (async () => { visit(); await a11yAudit24(); })()"
+    },
+    {
+      code: `import a11yAudit24 from 'ember-a11y-testing/test-support/audit'; (async () => { visit(); await a11yAudit24; })()`,
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018',
+        sourceType: 'module'
+      },
+      errors: [{ messageId: 'a11yAuditMustBeCalled' }],
+      output: "import a11yAudit24 from 'ember-a11y-testing/test-support/audit'; (async () => { visit(); await a11yAudit24(); })()"
+    },
+    // referencing without calling a11yAudit after
+    {
+      code: 'visit(); a11yAudit;',
+      errors: [{ messageId: 'a11yAuditMustBeCalled' }],
+      filename: TEST_FILE_NAME,
+      output: 'visit(); a11yAudit();'
+    },
+    // async function
+    {
+      code: '(async function () { visit(); a11yAudit; })();',
+      errors: [{messageId: 'a11yAuditMustBeCalled'}],
+      output: '(async function () { visit(); await a11yAudit(); })();',
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018'
+      },
+    },
+    // async function with block statements
+    {
+      code: '(async function () { while(true) { visit(); a11yAudit; } })();',
+      errors: [{messageId: 'a11yAuditMustBeCalled'}],
+      output: '(async function () { while(true) { visit(); await a11yAudit(); } })();',
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018'
+      },
+    },
+    // async arrow function
+    {
+      code: '(async () => { while(true) { visit(); a11yAudit; } })();',
+      errors: [{messageId: 'a11yAuditMustBeCalled'}],
+      output: '(async () => { while(true) { visit(); await a11yAudit(); } })();',
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018'
+      },
+    },
+    // custom module used
+    {
+      code: 'import { audit } from "dashboard/tests/helpers/audit"; (async () => { while(true) { visit(); audit; } })();',
+      errors: [{messageId: 'a11yAuditMustBeCalled'}],
+      output: 'import { audit } from "dashboard/tests/helpers/audit"; (async () => { while(true) { visit(); await audit(); } })();',
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018',
+        sourceType: 'module'
+      },
+      options: [
+        {
+          auditModule: {
+            package: 'dashboard/tests/helpers/audit',
+            exportName: 'audit'
+          }
+        }
+      ]
+    },
+    // custom module used, default import specifier
+    {
+      code: 'import audit from "dashboard/tests/helpers/audit"; (async () => { while(true) { visit(); audit; } })();',
+      errors: [{messageId: 'a11yAuditMustBeCalled'}],
+      output: 'import audit from "dashboard/tests/helpers/audit"; (async () => { while(true) { visit(); await audit(); } })();',
+      filename: TEST_FILE_NAME,
+      parserOptions: {
+        ecmaVersion: '2018',
+        sourceType: 'module'
+      },
+      options: [
+        {
+          auditModule: {
+            package: 'dashboard/tests/helpers/audit',
+            exportName: 'default'
+          }
+        }
+      ]
+    }
+  ]
+});
