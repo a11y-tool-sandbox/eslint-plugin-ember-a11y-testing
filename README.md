98e964a979f1b183b98cae533100fd5b20f3b189
Stanley Stuart <sstuart@salesforce.com>
2020-05-01 15:07:21 -0700

    move include/exclude settings to markdown file and document them

--- README.md
+++ README.md
@@ -2,7 +2,32 @@
 
 [![CircleCI Build Status](https://circleci.com/gh/a11y-tool-sandbox/eslint-plugin-ember-a11y-testing.svg?style=svg)](https://circleci.com/gh/a11y-tool-sandbox/eslint-plugin-ember-a11y-testing)
 
-ESLint plugin for ember-a11y-testing
+ESLint plugin for ember-a11y-testing. This plugin adds some rules to ensure
+that `a11yAudit` from
+[https://github.com/ember-a11y/ember-a11y-testing](https://github.com/ember-a11y/ember-a11y-testing)
+is called after each call to a helper from [@ember/test-helpers](https://github.com/emberjs/ember-test-helpers/blob/master/API.md).
+
+For example, the following code will trigger a linting error:
+
+```javascript
+import { module, test } from 'qunit';
+import { vist, click, setupAcceptanceTest } from '@ember/test-helpers';
+import a11yAudit from 'ember-a11y-testing';
+
+module('visiting the bakery', function(hooks) {
+  test('shows kolaches available', async (assert) {
+    await visit('/bakery');
+    await a11yAudit(); // no lint error here
+    await click('#kolache-button');
+    // oops, forgot to call a11yAudit here. ESLint will raise an error
+  });
+});
+```
+
+This ESLint plugin provides both reporting and autofixing (through `eslint
+--fix` or editor integration) and can serve as a tool to help you migrate to
+auditing your tests with [aXe](https://github.com/dequelabs/axe-core) through
+`ember-a11y-testing`.
 
 ## Installation
 
@@ -60,9 +85,86 @@ Or configure the rules you want to use under the rules section.
 ```json
 {
   "rules": {
-    "ember-a11y-testing/a11y-audit": "error"
+    "ember-a11y-testing/a11y-audit": "error",
+    "ember-a11y-testing/a11y-audit-after-test-helper": "error",
+    "ember-a11y-testing/a11y-audit-no-expression": "error",
+    "ember-a11y-testing/a11y-audit-no-globals": "error",
   }
 }
+```
+
+There are a few rules in this plugin to facilitate `eslint --fix`, so we
+recommend you keep all of them on.
+
+### Cofniguring which helpers to assert audit after
+
+By default, eslint-plugin-ember-a11y-testing will ensure there is a call to
+`a11yAudit` after this subset of helpers from
+[@ember/test-helpers](https://github.com/emberjs/ember-test-helpers/blob/master/API.md):
+
+- `visit`
+- `blur`
+- `click`
+- `doubleClick`
+- `fillIn`
+- `focus`
+- `tap`
+- `triggerEvent`
+- `triggerKeyEvent`
+- `typeIn`
+- `render`
+
+If you want to exclude any of these helpers for any reason, you can configure the `a11y-audit-after-test-helper` plugin as follows:
+
+```json
+{
+  "plugins": ["ember-a11y-testing"],
+  "settings": {
+    "ember-a11y-testing": {
+      "modules": {
+        "@ember/test-helpers": {
+          "exclude": [
+            "fillIn"
+          ]
+        }
+      }
+    }
+  }
+}
+```
+
+### Auditing custom helpers
+
+Apps and addons often develop their own helpers for interacting with components. eslint-plugin-ember-a11y-testing can audit those as well by specifying them in the `modules` setting. For example, if you a custom helper exported at `confirm` from the `tests/helpers/confirm` module, and the name of your app (as specified in `name` in package.json at the root of your project) is 'myapp':
+
+```json
+{
+  "plugins": ["ember-a11y-testing"],
+  "settings": {
+    "ember-a11y-testing": {
+      "modules": {
+        "myapp/tests/helpers": {
+          "include": ["confirm"]
+        }
+      }
+    }
+  }
+}
+```
+
+This will result in the following code trigger a linting error (and can also be autofixed if you have `eslint --fix` enabled):
+
+```
+import { confirm } from 'myapp/tests/helpers';
+
+module('my acceptance test', function(hooks) {
+  setupApplicationTest(hooks);
+  test('user can confirm thing', function(assert) => {
+    await visit('/seize-the-means-of-production');
+    await confirm('[data-test-selector="confirm-button"]');
+    // eslint will indicate an error here until away `a11yAudit` is added.
+  });
+});
 ```
 
 ### Using a custom audit module
--- tests/lib/rules/a11y-audit-after-test-helper.js
+++ tests/lib/rules/a11y-audit-after-test-helper.js
@@ -19,152 +19,202 @@ const { RuleTester } = require("eslint/lib/rule-tester");
 const TEST_FILE_NAME = "tests/acceptance/application-test.js";
 const ruleTester = new RuleTester();
 
-ruleTester.run("a11y-audit-after-test-helper", rule, {
+function runWithModernSyntax(testName, rule, options) {
+  const makeTestModern = (testCase) => {
+    const defaultParserOptions = {
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+      },
+    };
+    return {
+      ...defaultParserOptions,
+      ...testCase,
+    };
+  };
+  const validCases = (options.valid || []).map(makeTestModern);
+  const invalidCases = (options.invalid || []).map(makeTestModern);
+  return ruleTester.run(testName, rule, {
+    ...options,
+    valid: validCases,
+    invalid: invalidCases,
+  });
+}
+
+runWithModernSyntax("a11y-audit-after-test-helper", rule, {
   valid: [
     // visit
     {
-      code: `visit(); a11yAudit();`,
+      code: `
+      import { visit } from '@ember/test-helpers';
+      visit();
+      a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
     {
-      code: `visit();
+      code: `
+      import { visit } from '@ember/test-helpers';
+      visit();
       a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
 
     // rule not applicable outside of tests/acceptance folder
     {
-      code: `visit();`,
+      code: `import { visit } from '@ember/test-helpers'; visit();`,
       filename: "app/controllers/application.js",
     },
 
     // rule not applicable in non-acceptance tests
     {
-      code: `visit();`,
+      code: `import { visit } from '@ember/test-helpers'; visit();`,
       filename: "tests/integration/my-test.js",
     },
 
     // rule not applicable if function is excluded
     {
-      code: `visit();`,
+      code: `import { visit } from '@ember/test-helpers'; visit();`,
       filename: TEST_FILE_NAME,
-      options: [
-        {
-          exclude: ["visit"],
+      settings: {
+        "ember-a11y-testing": {
+          modules: {
+            "@ember/test-helpers": {
+              exclude: ["visit"],
+            },
+          },
         },
-      ],
+      },
     },
 
     //
     // smoke tests on other default test helpers
     //
     {
-      code: `blur(); a11yAudit();`,
+      code: `import { blur } from '@ember/test-helpers'; blur(); a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
     {
-      code: `click(); a11yAudit();`,
+      code: `import { click } from '@ember/test-helpers'; click(); a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
     {
-      code: `doubleClick(); a11yAudit();`,
+      code: `import { doubleClick } from '@ember/test-helpers'; doubleClick(); a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
     {
-      code: `focus(); a11yAudit();`,
+      code: `import { focus } from '@ember/test-helpers'; focus(); a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
     {
-      code: `tap(); a11yAudit();`,
+      code: `import { tap } from '@ember/test-helpers'; tap(); a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
     {
-      code: `triggerEvent(); a11yAudit();`,
+      code: `import { triggerEvent } from '@ember/test-helpers'; triggerEvent(); a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
     {
-      code: `triggerKeyEvent(); a11yAudit();`,
+      code: `import { triggerKeyEvent } from '@ember/test-helpers'; triggerKeyEvent(); a11yAudit();`,
       filename: TEST_FILE_NAME,
     },
     {
-      code: `async function foo() { await triggerKeyEvent(); await a11yAudit(); }`,
+      code: `import { triggerKeyEvent } from '@ember/test-helpers'; async function foo() { await triggerKeyEvent(); await a11yAudit(); }`,
       filename: TEST_FILE_NAME,
-      parserOptions: {
-        ecmaVersion: "2018",
-      },
     },
     // for of inside await
     {
-      code: `async function doStuff() {
+      code: `
+      import { click, blur } from '@ember/test-helpers';
+      async function doStuff() {
         for (const x of y) {
           await click(); a11yAudit();
           await blur(); a11yAudit();
         }
       }`,
       filename: TEST_FILE_NAME,
-      parserOptions: {
-        ecmaVersion: "2019",
-      },
     },
   ],
   invalid: [
     // nested block statements
     {
-      code: `async function doStuff() {
-        for await (const x of y) {
-          await fillIn(); await a11yAudit();
-          await blur('[data-test-selector]');
-        }
-      }`,
+      code: `import { blur, fillIn } from "@ember/test-helpers";
+        async function doStuff() {
+          for await (const x of y) {
+            await fillIn(); await a11yAudit();
+            await blur('[data-test-selector]');
+          }
+        }`,
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `async function doStuff() {
-        for await (const x of y) {
-          await fillIn(); await a11yAudit();
-          await blur('[data-test-selector]'); await a11yAudit();
-        }
-      }`,
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module",
-      },
+      output: `import { blur, fillIn } from "@ember/test-helpers";
+        async function doStuff() {
+          for await (const x of y) {
+            await fillIn(); await a11yAudit();
+            await blur('[data-test-selector]'); await a11yAudit();
+          }
+        }`,
+    },
+    // renaming ember test helper using import { helper as otherVariable }
+    {
+      code: `import { blur as blur2 /* woo hoo */, fillIn } from "@ember/test-helpers";
+        async function doStuff() {
+          for await (const x of y) {
+            await fillIn(); await a11yAudit();
+            await blur2('[data-test-selector]');
+          }
+        }`,
+      errors: [{ messageId: "a11yAuditAfterHelper" }],
+      filename: TEST_FILE_NAME,
+      output: `import { blur as blur2 /* woo hoo */, fillIn } from "@ember/test-helpers";
+        async function doStuff() {
+          for await (const x of y) {
+            await fillIn(); await a11yAudit();
+            await blur2('[data-test-selector]'); await a11yAudit();
+          }
+        }`,
     },
     // doesn't try to autofix if passed to function
     {
-      code: `assert.throws(fillIn('foo', 'bar'));`,
+      code: `import { fillIn } from "@ember/test-helpers"; assert.throws(fillIn('foo', 'bar'));`,
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `assert.throws(fillIn('foo', 'bar'));`,
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module",
-      },
+      output: `import { fillIn } from "@ember/test-helpers"; assert.throws(fillIn('foo', 'bar'));`,
     },
     // without adding a11yAudit after using `include` option
     {
-      code: "myCustom();",
-      options: [
-        {
-          include: ["myCustom"],
+      code: 'import {myCustom} from "custom"; myCustom();',
+      settings: {
+        "ember-a11y-testing": {
+          modules: {
+            custom: {
+              include: ["myCustom"],
+            },
+          },
         },
-      ],
+      },
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `myCustom(); a11yAudit();`,
+      output: `import {myCustom} from "custom"; myCustom(); a11yAudit();`,
     },
     // without adding a11yAudit after using `include` option (multiple)
     {
       code: `
+        import { myCustom, anotherCustom } from 'custom';
         myCustom();
         a11yAudit();
 
         anotherCustom();`,
-      options: [
-        {
-          include: ["myCustom", "anotherCustom"],
+      settings: {
+        "ember-a11y-testing": {
+          modules: {
+            custom: {
+              include: ["myCustom", "anotherCustom"],
+            },
+          },
         },
-      ],
+      },
       output: `
+        import { myCustom, anotherCustom } from 'custom';
         myCustom();
         a11yAudit();
 
@@ -172,55 +222,96 @@ ruleTester.run("a11y-audit-after-test-helper", rule, {
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
     },
+    // using custom helper with ember test helpers
+    {
+      code: `
+            import { myCustom, anotherCustom } from 'custom';
+            import { click } from '@ember/test-helpers';
+            myCustom();
+            a11yAudit();
+            click();
+
+            anotherCustom();`,
+      settings: {
+        "ember-a11y-testing": {
+          modules: {
+            custom: {
+              include: ["myCustom", "anotherCustom"],
+            },
+          },
+        },
+      },
+      output: `
+            import { myCustom, anotherCustom } from 'custom';
+            import { click } from '@ember/test-helpers';
+            myCustom();
+            a11yAudit();
+            click(); a11yAudit();
+
+            anotherCustom(); a11yAudit();`,
+      errors: [
+        { messageId: "a11yAuditAfterHelper" },
+        { messageId: "a11yAuditAfterHelper" },
+      ],
+      filename: TEST_FILE_NAME,
+    },
 
     //
     // smoke tests on other default test helpers
     //
-
     {
-      code: "blur();",
+      code: 'import { blur } from "@ember/test-helpers"; blur();',
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `blur(); a11yAudit();`,
+      output:
+        'import { blur } from "@ember/test-helpers"; blur(); a11yAudit();',
     },
     {
-      code: "click();",
+      code: 'import { click } from "@ember/test-helpers"; click();',
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `click(); a11yAudit();`,
+      output:
+        'import { click } from "@ember/test-helpers"; click(); a11yAudit();',
     },
     {
-      code: "doubleClick();",
+      code: 'import { doubleClick } from "@ember/test-helpers"; doubleClick();',
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `doubleClick(); a11yAudit();`,
+      output:
+        'import { doubleClick } from "@ember/test-helpers"; doubleClick(); a11yAudit();',
     },
     {
-      code: "focus();",
+      code: 'import { focus } from "@ember/test-helpers"; focus();',
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `focus(); a11yAudit();`,
+      output:
+        'import { focus } from "@ember/test-helpers"; focus(); a11yAudit();',
     },
     {
-      code: "tap();",
+      code: 'import { tap } from "@ember/test-helpers"; tap();',
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `tap(); a11yAudit();`,
+      output: 'import { tap } from "@ember/test-helpers"; tap(); a11yAudit();',
     },
     {
-      code: "triggerEvent();",
+      code:
+        'import { triggerEvent } from "@ember/test-helpers"; triggerEvent();',
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `triggerEvent(); a11yAudit();`,
+      output:
+        'import { triggerEvent } from "@ember/test-helpers"; triggerEvent(); a11yAudit();',
     },
     {
-      code: "triggerKeyEvent();",
+      code:
+        'import { triggerKeyEvent } from "@ember/test-helpers"; triggerKeyEvent();',
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
-      output: `triggerKeyEvent(); a11yAudit();`,
+      output:
+        'import { triggerKeyEvent } from "@ember/test-helpers"; triggerKeyEvent(); a11yAudit();',
     },
     {
       code: `
+      import { visit } from '@ember/test-helpers';
       import a11yTesting24 from "ember-a11y-testing/test-support/audit";
       async function foo() {
         await visit();
@@ -228,14 +319,11 @@ ruleTester.run("a11y-audit-after-test-helper", rule, {
       errors: [{ messageId: "a11yAuditAfterHelper" }],
       filename: TEST_FILE_NAME,
       output: `
+      import { visit } from '@ember/test-helpers';
       import a11yTesting24 from "ember-a11y-testing/test-support/audit";
       async function foo() {
         await visit(); await a11yTesting24();
       }`,
-      parserOptions: {
-        ecmaVersion: "2018",
-        sourceType: "module",
-      },
     },
   ],
 });
