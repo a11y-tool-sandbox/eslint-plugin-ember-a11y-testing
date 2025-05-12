7f238e692ac08a0d32eb2fad56aee9b3869e7c59
Dean Marano <dean.marano@hashicorp.com>
2025-05-12 11:56:04 -0400

    fix: update ESLint configuration for flat config compatibility

--- recommended.mjs
+++ recommended.mjs
@@ -1,27 +1,3 @@
-import * as emberA11yTesting from "./lib/index.js";
+import emberA11yTesting from "./lib/index.mjs";
 
-export default {
-  languageOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-    globals: {
-      browser: true,
-    },
-  },
-  plugins: {
-    "ember-a11y-testing": emberA11yTesting,
-  },
-  settings: {
-    "ember-a11y-testing": {
-      auditModule: {
-        package: "ember-a11y-testing/test-support/audit",
-        exportName: "default",
-      },
-    },
-  },
-  rules: {
-    "ember-a11y-testing/a11y-audit-after-test-helper": "error",
-    "ember-a11y-testing/a11y-audit-no-expression": "error",
-    "ember-a11y-testing/a11y-audit-no-globals": "error",
-  },
-};
+export default emberA11yTesting.configs.recommended;
